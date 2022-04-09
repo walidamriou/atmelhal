1b72a99f3e92c26c1d0c38a78533fef1046ad969
Walid Amriou <walidamriou@gmail.com>
2022-04-09 11:21:47 +0100

    Update hal_serial_user.h

---
+++ hal_serial_user.h
@@ -0,0 +1,40 @@
+/********************************************************************************\ 
+ @file hal_serial_user.h 
+ @brief header file of serial receive handler
+ @version 1.0.0
+ @author Walid Amriou 
+ @date 09/04/2022
+
+* This work is licensed under:
+Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License
+
+* You are free to:
+Share copy and redistribute the material in any medium or format
+
+* Under the following terms:
+- Attribution You must give appropriate credit, provide a link to the license, and 
+indicate if changes were made. You may do so in any reasonable manner, but not in 
+any way that suggests the licensor endorses you or your use.
+- NonCommercial You may not use the material for commercial purposes.
+- NoDerivatives If you remix, transform, or build upon the material, you may not 
+distribute the modified material.
+- No additional restrictions You may not apply legal terms or technological measures 
+that legally restrict others from doing anything the license permits.
+
+the software or Code or Scripts or any files in this source is provided "as is" and 
+the author disclaims all warranties with regard to this files including all implied 
+warranties of merchantability and fitness. in no event shall the author be liable 
+for any special, direct, indirect, or consequential damages or any damages whatsoever 
+resulting from loss of use, data or profits, whether in an action of contract, 
+negligence or other tortious action, arising out of or in connection with the use or 
+performance of this software or code or scripts or any files in this source.
+
+\********************************************************************************/
+
+#include "hal_ global.h"
+
+#ifndef HAL_SERIAL_USER_H
+#define HAL_SERIAL_USER_H
+
+
+#endif
